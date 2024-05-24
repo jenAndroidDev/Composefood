0db29c13ff8208add52e71f6f5356b5ce0936592
jenAndroidDev <jendev971@gmail.com>
2024-05-24 08:24:42 +0530

    details image

--- app/src/main/java/com/example/composefood/feature/detail/presentation/FoodDetailScreen.kt
+++ app/src/main/java/com/example/composefood/feature/detail/presentation/FoodDetailScreen.kt
@@ -1,13 +1,19 @@
 package com.example.composefood.feature.detail.presentation
 
 import androidx.compose.animation.core.exponentialDecay
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
@@ -18,20 +24,30 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composefood.R
 import com.example.composefood.components.HeaderBackIcon
 import com.example.composefood.components.ProfileIcon
+import com.example.composefood.ui.theme.GoldenYellow
+import com.example.composefood.ui.theme.PaleWhite
 
 @Composable
 fun FoodDetailScreen(
     modifier: Modifier = Modifier,
     onClick:()->Unit
 ){
-    Surface {
+    Surface(modifier = modifier.fillMaxSize()) {
         Column {
             Spacer(modifier = modifier.height(16.dp))
             Header()
+            Spacer(modifier = modifier.height(16.dp))
+            ContentImage()
         }
     }
 }
@@ -50,6 +66,35 @@ private fun Header(modifier: Modifier = Modifier){
             HeaderBackIcon(icon = Icons.Default.KeyboardArrowLeft)
         }
         ProfileIcon(modifier = modifier, imageVector = Icons.Default.FavoriteBorder) }
+}
+
+@Preview
+@Composable
+private fun ContentImage(modifier: Modifier = Modifier){
+
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .height(200.dp)
+        .background(color = PaleWhite),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Spacer(modifier = modifier.height(12.dp))
+        Image(
+            painter = painterResource(id = R.drawable.item_b),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .shadow(
+                    shape = CircleShape,
+                    elevation = 4.dp,
+                    ambientColor = GoldenYellow,
+                    spotColor = GoldenYellow
+                )
+                .clip(CircleShape)
+        )
+    }
 }
 
 @Preview
