3d0401b714637952f5773ebc9019ad4cd0f0d7e6
jenAndroidDev <jendev971@gmail.com>
2024-06-02 12:17:32 +0530

    make detail screen scrollable

--- app/src/main/java/com/example/composefood/feature/detail/presentation/FoodDetailScreen.kt
+++ app/src/main/java/com/example/composefood/feature/detail/presentation/FoodDetailScreen.kt
@@ -1,13 +1,11 @@
 package com.example.composefood.feature.detail.presentation
 
-import android.widget.Space
+
 import androidx.compose.animation.AnimatedContentScope
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.animation.core.exponentialDecay
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -16,32 +14,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -68,14 +60,20 @@ fun FoodDetailScreen(
     itemId: Int,
     onClick: () -> Unit
 ) {
+    /*
+    TODO Collapsing tool bar layout. and make list scrollable. Ingredients
+
+    * */
     Surface(
         modifier = modifier
             .fillMaxSize()
 
     ) {
         with(sharedTransitionScope) {
+            val scrollState = rememberScrollState()
             Column(
                 modifier = modifier
+                    .verticalScroll(scrollState)
                     .background(color = Color.White)
             ) {
                 Header(onClick = onClick)
@@ -213,13 +211,4 @@ private fun FoodDetailsContent(modifier: Modifier){
 @Composable
 fun IngredientsList() {
 
-}
-
-@OptIn(ExperimentalSharedTransitionApi::class)
-@Composable
-fun SharedTransitionScope.PreviewDetailsScreen() {
-
-    SharedTransitionLayout {
-
-    }
 }
